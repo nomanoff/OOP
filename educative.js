36c92a28c06dff84cdb40e3d6261654fabdbca44
aliabdulazeez <aliabdulazeez01@gmail.com>
2021-01-17 17:13:05 +0900

    new problem and its solution added

--- educative.js
+++ educative.js
@@ -7,7 +7,7 @@ var employee = {
   designation : 'Developer',
   //function to display name of the employee
   displayName() {
-    console.log("Name is:", this.name)
+    // console.log("Name is:", this.name)
   }
 }
 //calling the method
@@ -67,12 +67,12 @@ function Employee(name, age, designation){
 }
 var employeeObj = new Employee('Joe',20,'Chef')
 //accessing properties outside
-console.log(employeeObj._name);
-console.log(employeeObj._age);
-console.log(employeeObj._designation);
-console.log(employeeObj.getName());
-console.log(employeeObj.setAge('John'));
-console.log(employeeObj.getName());
+// console.log(employeeObj._name);
+// console.log(employeeObj._age);
+// console.log(employeeObj._designation);
+// console.log(employeeObj.getName());
+// console.log(employeeObj.setAge('John'));
+// console.log(employeeObj.getName());
 
 
 /*
@@ -106,4 +106,74 @@ function Parallelogram(base, height){
 }
 
 var parallelogram = new Parallelogram(4,5);
-console.log(parallelogram.printArea());
+// console.log(parallelogram.printArea());
+
+
+
+/*
+Define a static function checkExpiry(product1,product2). Here’s what it should do:
+
+It should take two product objects of the same type but from different brands as parameters.
+
+It should compare their expiryDate with the current date.
+
+Based on the difference of the expiryDate from the current date it should return which brand’s product to buy, i.e., return the brand property of that product.
+
+It should return Neither if both products have expired.
+
+It should return brand property of product1 if product2 has expired, but product1 hasn’t.
+
+It should return brand property of product2 if product1 has expired, but product2 hasn’t.
+
+If neither of the products has expired it should compute the difference of both of their expiry dates from the current date and return the brand of the product whose difference is greater.
+
+It should return Either if both products have the same expiry date.
+
+*/
+
+
+
+
+class Product{  
+  //write code here
+  constructor(name, price, amount, madeIn, expiryDate, brand){
+  this.name = name      
+  this.price = price
+  this.amount = amount
+  this.madeIn = madeIn
+  this.expiryDate = expiryDate
+  this.brand = brand
+
+  }
+
+  static checkExpiry(product1,product2){
+     const currentDate = new Date();
+     
+     if(product1.expiryDate < currentDate && product2.expiryDate < currentDate) {
+        return console.log('Neither');
+     }else if(product1.expiryDate > product2.expiryDate){
+        return product1.brand;
+     }else if(product1.expiryDate < product2.expiryDate){
+        return product2.brand;
+     }else{
+       return console.log('Either');
+     }
+
+  }
+}
+
+
+
+
+
+let today = new Date('2021-02-17');
+let yesterday = new Date('2021-01-27');
+
+if( +today > +yesterday){
+  console.log("today");
+}else{
+  console.log("yesterday");
+}
+
+console.log(today);
+console.log(yesterday);
